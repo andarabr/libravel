ec28ac0457ec5eba56d63909c63fd93724690397
unknown <andarabrey@gmail.com>
2021-01-30 19:36:11 +0700

    add Paper CRUD

---
+++ database/migrations/2021_01_30_081008_create_papers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePapersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('papers', function (Blueprint $table) {
+            $table->id();
+            $table->char('papid', 100)->unique();
+            $table->char('title', 100);
+            $table->char('author', 50);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('papers');
+    }
+}
---
+++ resources/views/paper/edit.blade.php
@@ -0,0 +1,34 @@
+@extends('layout.master')
+
+@section('content')
+    <div class="container pt-4 bg-white">
+        <div class="row">
+            <div class="col-md-12 col-xl-12">
+                <h1>Edit Paper {{$paper->papid}}</h1>
+                <form action="{{ route('papers.update', $paper->papid) }}" method="POST">
+                    @method('PATCH')
+                    @csrf
+                    <div class="form-group">
+                        <label for="papid">Paper ID</label>
+                        <input type="text" class="form-control" name="papid" value="{{$paper->papid}}" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="title">Paper Title</label>
+                        <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{$paper->title}}">
+                        @error('title')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="papid">Author</label>
+                        <input type="text" class="form-control @error('author') is-invalid @enderror" name="author" value="{{$paper->author}}">
+                        @error('author')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn btn-primary">Update</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
