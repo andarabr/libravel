765072d5b6e5b55f9df7d85c0bfafd8586bed0d0
unknown <andarabrey@gmail.com>
2021-01-30 19:36:00 +0700

    add Paper CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaperController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [paperController::class, 'index'])->name('papers.index');
+
+//--------------------------------------------------------------------------
+
+Route::get('/papers', [paperController::class, 'index'])->name('papers.index');
+
+Route::get('/papers/create', [paperController::class, 'create'])->name('papers.create');
+
+Route::post('/papers/store', [paperController::class, 'store'])->name('papers.store');
+
+Route::get('/papers/{papid}', [paperController::class, 'detail'])->name('papers.detail');
+
+Route::get('/papers/{papid}/edit', [paperController::class, 'edit'])->name('papers.edit');
+
+Route::patch('/papers/{papid}', [paperController::class, 'update'])->name('papers.update');
+
+Route::delete('/papers/{papid}', [paperController::class, 'destroy'])->name('papers.destroy');
+
+
+
+
